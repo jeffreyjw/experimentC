b24f9cd0af96531f4ab7a0e4fed65990829185d2
Tomasz Kasperek <kasperekt@gmail.com>
2014-09-19 18:27:48 +0200

    Zadanie 3

--- scripts/gaslib/Collection.js
+++ scripts/gaslib/Collection.js
@@ -1,6 +1,6 @@
 define([
-    
-], function(){
+    "gaslib/Iterator"
+], function(Iterator){
 
     var Collection = function(){
         this.__collection = [];
@@ -12,6 +12,11 @@ define([
 
     Collection.prototype.add = function(element){
         this.__collection.push(element);
+    };
+
+    Collection.prototype.getIterator = function() {
+      iterator = new Iterator(this.__collection);
+      return iterator;
     };
 
 
---
+++ testorial/tests/Collection/script.js
@@ -0,0 +1,16 @@
+require([
+    "gaslib/Collection"
+], function(Collection){
+
+    QUnit.test( "Collection example", function( assert ) {
+        var collection = new Collection();
+
+        iterator = collection.getIterator();
+        position = iterator.position;
+        assert.equal(position, -1, 'Iterator should be on -1 position')
+
+    });
+
+});
+
+
